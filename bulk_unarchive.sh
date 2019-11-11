512e70a302ccd2b8bf49215c8dea31a8b5306390
Alex Costa <acostabh@gmail.com>
2019-11-11 09:57:51 -0800

    Updated comments

--- bulk_unarchive.sh
+++ bulk_unarchive.sh
@@ -4,7 +4,7 @@ token=$(jq -r ".token" token.json)
 export IFS=","
 cat ML_Archived_Projects.csv | while read f1 f2 f3 f4
   do
-    #discard first line based on the value of field 1 project_id
+    #discard top line based on the value of field 1 (project_id) and iterate through the remaining lines/rows
     if [ $f1 != "project_id" ]; then
       curl -X PUT -H 'Authorization: Bearer '$token -H "Accept: Application/json" -H "Content-Type: application/json" "https://api.mavenlink.com/api/v1/workspaces/"$f1 -d '{ "workspace": {	"archived": false }}' >> $(date +%Y"-"%m"-"%d)_log.txt
     fi
